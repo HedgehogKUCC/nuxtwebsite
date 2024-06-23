b65623988d848476a7105b74760e486a7be325a1
CHAN-CHENG KU <ccc908925@gmail.com>
2024-06-23 15:14:20 +0800

    feat: mobile header

--- components/Header.vue
+++ components/Header.vue
@@ -47,12 +47,13 @@ const scrollToHref = (e: Event) => {
 
 <template>
 <header class="header">
-  <div class="container d-flex align-items-center">
+  <input type="checkbox" name="mobile-menu" id="mobile-menu" class="mobile-menu" />
+  <div class="container header-wrap">
     <a href="/" title="回首頁" class="header-logo">
       <DotLottieVue style="width: 100px; height: 100px;" autoplay loop src="https://lottie.host/c1858a42-23f4-4b0b-8655-2b07726b2c95/TSJoSaZ0Sw.json" />
       <el-text tag="h1">展誠</el-text>
     </a>
-    <nav class="d-flex">
+    <nav class="desktop-menu-list">
       <a href="/" :class="{ active: fullPath === '/' }">
         首頁
         <span class="fake top-line"></span>
@@ -79,7 +80,48 @@ const scrollToHref = (e: Event) => {
         <span class="fake bottom-line"></span>
       </a>
     </nav>
+    <label for="mobile-menu" class="mobile-menu-label">
+      <BootstrapIcon name="list" />
+      <BootstrapIcon name="x" />
+    </label>
   </div>
+  <ul class="mobile-menu-list">
+    <li>
+      <a href="/" :class="{ active: fullPath === '/' }">
+        首頁
+        <span class="fake top-line"></span>
+        <span class="fake bottom-line"></span>
+      </a>
+    </li>
+    <li>
+      <a href="/works" class="three-word" @click.prevent="handlerClick">
+        作品集
+        <span class="fake top-line"></span>
+        <span class="fake bottom-line"></span>
+      </a>
+    </li>
+    <li>
+      <a href="https://medium.com/conrad-ku" class="three-word" target="_blank">
+        部落格
+        <span class="fake top-line"></span>
+        <span class="fake bottom-line"></span>
+      </a>
+    </li>
+    <li>
+      <a href="#service" class="four-word" @click.prevent="scrollToHref">
+        服務項目
+        <span class="fake top-line"></span>
+        <span class="fake bottom-line"></span>
+      </a>
+    </li>
+    <li>
+      <a href="mailto:ccc908925@gmail.com#" class="three-word">
+        聯絡我
+        <span class="fake top-line"></span>
+        <span class="fake bottom-line"></span>
+      </a>
+    </li>
+  </ul>
 </header>
 </template>
 
@@ -88,8 +130,20 @@ const scrollToHref = (e: Event) => {
   border-bottom: solid 1px var(--cus-color-primary-200);
   box-shadow: var(--el-box-shadow-lighter);
 
+  &-wrap {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @include media-breakpoint-up(xl) {
+      justify-content: flex-start;
+    }
+  }
+
   &-logo {
-    margin-right: 318px;
+    @include media-breakpoint-up(xl) {
+      margin-right: 318px;
+    }
   }
 
   h1 {
@@ -100,76 +154,147 @@ const scrollToHref = (e: Event) => {
     border: 0;
   }
 
-  nav > a {
-    display: block;
-    flex-basis: content;
-    color: var(--cus-color-primary-900);
-    text-decoration: none;
-    position: relative;
-    padding: 8px 12px;
-    border: solid 1px transparent;
+  .bi-list::before {
+    font-size: 40px;
+  }
 
-    &:not(:last-child) {
-      margin-right: 40px;
+  .bi-x::before {
+    display: none;
+    font-size: 45px;
+  }
+
+  .desktop-menu-list,
+  .mobile-menu-list {
+    a {
+      display: block;
+      flex-basis: content;
+      color: var(--cus-color-primary-900);
+      text-decoration: none;
+      position: relative;
+      padding: 8px 12px;
+      border: solid 1px transparent;
+
+      &.active,
+      &:hover {
+        border: solid 1px var(--cus-color-primary-500);
+
+        &::before,
+        &::after,
+        > .fake {
+          content: '';
+          position: absolute;
+          height: 1px;
+          background: #FFFFFF;
+        }
+
+        &::before,
+        &::after {
+          width: 19px;
+          top: 8px;
+        }
+
+        &::before {
+          transform-origin: 0 0 0;
+          transform: rotate(90deg);
+          left: 0;
+        }
+
+        &::after {
+          transform-origin: 100% 100% 0;
+          transform: rotate(-90deg);
+          right: -1px;
+        }
+
+        > .top-line,
+        > .bottom-line {
+          width: 42px;
+          left: 7px;
+        }
+
+        &.three-word {
+          > .top-line,
+          > .bottom-line {
+            width: 58px;
+          }
+        }
+
+        &.four-word {
+          > .top-line,
+          > .bottom-line {
+            width: 74px;
+          }
+        }
+
+        > .top-line {
+          top: -1px;
+        }
+
+        > .bottom-line {
+          bottom: -1px;
+        }
+      }
+    }
+  }
+
+  .mobile-menu {
+    display: none;
+
+    &-label {
+      display: inline;
+
+      @include media-breakpoint-up(xl) {
+        display: none;
+      }
     }
 
-    &.active,
-    &:hover {
-      border: solid 1px var(--cus-color-primary-500);
+    &-list {
+      margin: 0;
+      padding: 40px 0;
+      position: relative;
+      z-index: 100;
+      display: none;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      list-style: none;
+      border-top: solid 1px var(--cus-color-primary-200);
 
-      &::before,
-      &::after,
-      > .fake {
-        content: '';
-        position: absolute;
-        height: 1px;
-        background: #FFFFFF;
+      @include media-breakpoint-up(xl) {
+        display: none;
       }
 
-      &::before,
-      &::after {
-        width: 19px;
-        top: 8px;
+      li:not(:last-child) {
+        margin-bottom: 24px;
+      }
+    }
+
+    &:checked {
+      ~ .mobile-menu-list {
+        display: flex;
       }
 
-      &::before {
-        transform-origin: 0 0 0;
-        transform: rotate(90deg);
-        left: 0;
-      }
+      ~ .header-wrap {
+        .bi-list::before {
+          display: none;
+        }
 
-      &::after {
-        transform-origin: 100% 100% 0;
-        transform: rotate(-90deg);
-        right: -1px;
-      }
-
-      > .top-line,
-      > .bottom-line {
-        width: 42px;
-        left: 7px;
-      }
-
-      &.three-word {
-        > .top-line,
-        > .bottom-line {
-          width: 58px;
+        .bi-x::before {
+          display: inline-block;
         }
       }
+    }
+  }
 
-      &.four-word {
-        > .top-line,
-        > .bottom-line {
-          width: 74px;
-        }
-      }
+  .desktop-menu-list {
+    display: none;
 
-      > .top-line {
-        top: -1px;
-      }
+    @include media-breakpoint-up(xl) {
+      display: flex;
+    }
 
-      > .bottom-line {
-        bottom: -1px;
+    > a {
+      &:not(:last-child) {
+        margin-right: 40px;
       }
     }
   }
